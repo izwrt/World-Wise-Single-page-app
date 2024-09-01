f7187f22360fd73f49cc89217cb0a9f203c28ef9
izwrt <ishwart466@gmail.com>
2024-09-01 19:53:17 +0530

    css modules are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Pricing";
+
+const router = createBrowserRouter([
+  {
+    path : "/login",
+    element: <Login/>
+  },
+  {
+    path: "/home",
+    element: <Homepage/>
+  },
+  {
+    path: "/pricing",
+    element: <Pricing/>
+  },
+  {
+    path: "/product",
+    element: <Product/>
+  }
+]);
+
+
 const App = () => {
-    return (
-      <div>
-        <h1>hello world</h1>
-      </div>
-    );
-  };
+  return <RouterProvider router={router}/>
+}
   
-  export default App;
+export default App;
   
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import styles from "./Login.module.css";
+import { useState } from "react";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
