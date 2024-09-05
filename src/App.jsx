447a6c1cc5f7ba54940dc3feadf5286259526b5b
izwrt <ishwart466@gmail.com>
2024-09-05 14:08:06 +0530

    added new api

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,34 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
+import {useState, useEffect} from "react";
 import Product from "./pages/Pricing";
 import "./index.css"
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
+
+const BASE_URL = "http://localhost:8000/";
+
+const App = () => {
+
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try{
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("The was an error loading data");
+      } finally{
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  },[]);
 
 const router = createBrowserRouter([
   {
@@ -41,7 +65,7 @@ const router = createBrowserRouter([
 ]);
 
 
-const App = () => {
+
   return <RouterProvider router={router}/>
 }
   
