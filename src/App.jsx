9b0b5a6e78321b17aa8e0ba1e3c0605df4641ca7
izwrt <ishwart466@gmail.com>
2024-09-06 22:02:51 +0530

    old style routing by adding children routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, RouterProvider,Routes,Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
@@ -12,61 +12,23 @@ const BASE_URL = "http://localhost:8000/";
 
 const App = () => {
 
-  const [cities, setCities] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+return(
+  <BrowserRouter>
+    <Routes>
+      <Route index element ={<Homepage/>}/>
+    </Routes>
+      <Route index element={<Homepage />} />
+      <Route path="product" element={<Product />} />
+      <Route path="pricing" element={<Pricing />} />
+      <Route path="login" element={<Login />} />
+      <Route path="app" element={<AppLayout/>}>
+        <Route path="cities" element={<CityList/>}/>
+        <Route paht="coutries" element={<p>Coutrues</p>}/>
+      </Route>
 
-  useEffect(function () {
-    async function fetchCities() {
-      try{
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("The was an error loading data");
-      } finally{
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  },[]);
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Homepage/>
-  },
-  {
-    path : "/login",
-    element: <Login/>
-  },
-  {
-    path: "/pricing",
-    element: <Pricing/>
-  },
-  {
-    path: "/product",
-    element: <Product/>
-  },
-  {
-    path: "/app",
-    element: <AppLayout/>,
-    children: [
-      {
-      path: "cities",
-      element: <CityList/>
-    },
-    {
-      path: "countries",
-      element: <p>countries</p>
-    },
-  ]
-  },
-]);
-
-
-
-  return <RouterProvider router={router}/>
+  
+  </BrowserRouter>
+)
 }
   
 export default App;
