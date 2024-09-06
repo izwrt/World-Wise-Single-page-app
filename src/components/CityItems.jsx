ec81a6f5bbea599c460fd6b1167e2e79b6a261a9
izwrt <ishwart466@gmail.com>
2024-09-06 23:17:32 +0530

    CityList page created to display idual cities

---
+++ src/components/CityItems.jsx
@@ -0,0 +1,8 @@
+
+const CityItems = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,9 +1,13 @@
 import styles from './CityList.module.css';
+import Spinner from './Spinner';
+import CityItems from './CityItems';
 
-function CityList() {
+function CityList({isLoading,cities}) {
+    if(isLoading) return (<Spinner/> )
+
     return (
         <ul style={styles.CityList}>
-            List
+            {cities.map(city => <CityItems city={city}/>)}
         </ul>
     )
 }
