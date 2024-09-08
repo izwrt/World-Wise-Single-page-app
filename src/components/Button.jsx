1593c8cf981eafee8ce325d1cf62a8fad98f494f
izwrt <ishwart466@gmail.com>
2024-09-08 14:32:13 +0530

    implemented add and back button using Button component and used nagivate to angivate back page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,9 @@
-const Button = ({childrens}) => {
-    <button>
-        {childrens}
-    </button>
+import styles from "./Button.module.css"
+
+const Button = ({children ,type,onClik}) => {
+  return ( <button className={`${styles.btn} ${styles[type]}`} onClick={onClik}>
+        {children}
+    </button>)
 }
 
 export default Button;
