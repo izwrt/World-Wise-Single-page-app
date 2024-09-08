2a61a783dca33308db626b044343a728777477d4
izwrt <ishwart466@gmail.com>
2024-09-08 11:28:51 +0530

    URL ui is created

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import "./index.css"
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:8000/";
 
@@ -47,6 +48,7 @@ return(
       <Route path="app" element={<AppLayout/>}>
       <Route index element={<CityList cities={cities} isLoading={isLoading} />}/>
         <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />}/>
+        <Route path="cities/:id" element={<City/>}/>
         <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>}/>
         <Route path="form" element={<p>form</p>}/>
       </Route>
