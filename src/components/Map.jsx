a5f076d6b12b36b15e3967ca19537dcb1d7307cb
izwrt <ishwart466@gmail.com>
2024-10-29 19:31:33 +0530

    lat and lng added to map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,13 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvent } from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 
 
 function Map() {
 
-  const [searchParams,setSearchParams] = useSearchParams();
-  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
   const [mapPosition, setMapPosition] = useState([40,0])
   const { cities } = useCities();
 
@@ -16,9 +15,13 @@ function Map() {
   const lng = searchParams.get('lng');
   console.log(searchParams);
   
+  useEffect(function() {
+    if(lng && lat) setMapPosition([lat,lng])
+  },[lat,lng]);
+
   return(
     <div className={styles.mapContainer} >
-        <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+        <MapContainer className={styles.map} center={mapPosition} zoom={6} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
@@ -31,9 +34,26 @@ function Map() {
         </Popup>
       </Marker>
       ) )}
+
+      <ChangeCenter position={mapPosition }/>
+      <DetectClick/>
       </MapContainer>
     </div>
   )
+}
+
+function ChangeCenter({ position }){
+  const map = useMap();
+  map.setView(position);
+  return null
+}
+
+function DetectClick(){
+  const navigate = useNavigate();
+  
+  useMapEvent({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+  });
 }
 
 export default Map;
